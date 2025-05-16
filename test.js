6e77043ba39eea3974f281f7c19c6afe339c8d9e
karimait12 <karimaityahia98@gmail.com>
2025-05-16 19:25:44 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+import makeWASocket from '@itsukichan/baileys';
+
+async function testPair() {
+  const sock = makeWASocket({ printQRInTerminal: false });
+  try {
+    const code = await sock.requestPairingCode('212679894168');
+    console.log('Pairing code is:', code);
+  } catch (e) {
+    console.error('Failed to pair:', e);
+  }
+}
+testPair();￼Enter
